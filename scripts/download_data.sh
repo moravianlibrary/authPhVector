671fccca86ec316baef6dfd7d0698f6d565ccf39
Josef Moravec <josef.moravec@gmail.com>
2026-05-18 12:48:52 +0200

    Add geo authorities do download script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,5 +3,8 @@
 echo "Downloading data"
 wget "https://aleph.nkp.cz/data/aut_ph.xml.gz" -O aut_ph.xml.gz
 gunzip aut_ph.xml.gz
+wget "https://aleph.nkp.cz/data/aut_ge.xml.gz" -O aut_ge.xml.gz
+gunzip aut_ge.xml.gz
+
 
 echo "Done"
